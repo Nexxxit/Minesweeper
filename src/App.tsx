e97c4cd42315c9bf1379c4daca4a1e7ffcc223e9
Nexxxit <varsik.arsik@mail.ru>
2025-03-18 07:49:15 +0500

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Settings from './components/settings/Settings';
+import Menu from "./components/menu/Menu";
+import Leaderboard from "./components/leaderboard/Leaderboard";
+import Game from "./components/game/Game";
+
 export default function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Menu/>}/>
+        <Route path="/settings" element={<Settings/>}/>
+        <Route path="/leaderboard" element={<Leaderboard/>}/>
+        <Route path="/game" element={<Game/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
