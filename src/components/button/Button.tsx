05f7a44aed90df4f7adfcfbb0f3fbd3e21bd22e6
Nexxxit <varsik.arsik@mail.ru>
2025-03-19 21:39:47 +0500

    refactor: update menu and button components

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,23 +1,14 @@
-import { useNavigate } from "react-router-dom";
-
 interface ButtonProps {
   btnText: string;
   onClick?: () => void;
-  to?: string;
+  className?: string;
 }
 
-export default function Button({ btnText, onClick, to }: ButtonProps) {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    if (onClick) onClick();
-    if (to) navigate(to);
-  };
-
+export default function Button({ btnText, onClick, className }: ButtonProps) {
   return (
     <button
-      className="rounded-xl hover:outline-2 outline-offset-2 cursor-pointer p-2 shadow bg-yellow-300 hover:bg-yellow-400 text-center w-40 md:w-80 xl:w-160 text-md md:text-xl xl:text-2xl"
-      onClick={handleClick}
+      className={`${className} rounded-xl hover:outline-2 outline-offset-2 cursor-pointer p-2 shadow bg-yellow-300 hover:bg-yellow-400 text-center w-40 md:w-80 xl:w-160 text-md md:text-xl xl:text-2xl`}
+      onClick={onClick}
     >
       {btnText}
     </button>
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,15 +1,23 @@
 import Button from "../button/Button";
+import { Link } from "react-router-dom";
 
 export default function Menu() {
-
-    return(
-        <div className="flex flex-col items-center justify-center gap-5">
-            <div className="mb-20">
-                <p className="text-7xl text-yellow-300 font-bold">Minesweeper</p>
-            </div>
-            <Button btnText="Начать игру" to="/game"/>
-            <Button btnText="Таблица лидеров" to="leaderboard"/>
-            <Button btnText="Настройки" to="/settings"/>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center gap-5">
+      <div className="sm:mb-20 mb-5">
+        <p className="sm:text-7xl text-5xl text-yellow-300 font-bold ">
+          Minesweeper
+        </p>
+      </div>
+      <Link to={"/game"}>
+        <Button btnText="Начать игру" />
+      </Link>
+      <Link to={"/leaderboard"}>
+        <Button btnText="Таблица лидеров" />
+      </Link>
+      <Link to={"/settings"}>
+        <Button btnText="Настройки" />
+      </Link>
+    </div>
+  );
 }
