59d8ac8a47010e665fad2e2da71afe44ffb28d94
Nexxxit <varsik.arsik@mail.ru>
2025-03-19 22:10:54 +0500

    feat: add settingMode and settings

---
+++ src/components/settingMode/SettingMode.tsx
@@ -0,0 +1,35 @@
+import { ChangeEvent } from "react";
+
+interface SettingModeProps {
+  labelText: string;
+  check: boolean;
+  id: string;
+  handleOptionChange: (event: ChangeEvent<HTMLInputElement>) => void; 
+}
+
+export default function SettingMode({
+  labelText,
+  check,
+  id,
+  handleOptionChange
+}: SettingModeProps) {
+
+  return (
+    <div className="flex gap-3 w-60 md:w-80 xl:w-160">
+      <label
+        className="w-full flex items-center transition duration-150 ease-in-out lg:hover:-translate-y-1 lg:hover:scale-105  flex gap-3 rounded-xl has-checked:outline-2 has-checked:outline-solid hover:outline-2 hover:outline-dashed outline-offset-2 cursor-pointer bg-blue-400 hover:bg-blue-500 p-5 has-checked:bg-blue-500 text-md md:text-xl xl:text-2xl"
+        htmlFor={id}
+      >
+        <input
+          className="box-content h-1.5 w-1.5 appearance-none rounded-full border-[5px] border-white bg-white ring-1 ring-gray-950/20 outline-none checked:border-purple-500 checked:ring-purple-500"
+          type="radio"
+          name="settingMode"
+          id={id}
+          checked={check}
+          onChange={handleOptionChange}
+        />
+        {labelText}
+      </label>
+    </div>
+  );
+}
---
+++ src/components/settings/Settings.tsx
@@ -0,0 +1,49 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import Button from "../button/Button";
+import SettingMode from "../settingMode/SettingMode";
+import { Link } from "react-router-dom";
+
+export default function Settings() {
+    const [selectedOption, setSelectedOption] = useState<string>(() => {
+        return localStorage.getItem('selectedOption') || 'easy';
+    });
+
+    const handleOptionChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSelectedOption(event.target.id);
+    }
+
+    useEffect(() => {
+        localStorage.setItem('selectedOption', selectedOption);
+    }, [selectedOption])
+
+  return (
+    <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-5">
+        <SettingMode 
+          labelText="Просто 8х8, 10 мин."  
+          id="easy" 
+          check={selectedOption === 'easy'}
+          handleOptionChange={handleOptionChange} 
+        />
+        <SettingMode
+          labelText="Средний 16х16, 40 мин."
+          id="medium"
+          check={selectedOption === 'medium'}
+          handleOptionChange={handleOptionChange}
+        />
+        <SettingMode
+          labelText="Сложный 32х16, 100 мин."
+          id="hard"
+          check={selectedOption === 'hard'}
+          handleOptionChange={handleOptionChange}
+        />
+      </div>
+      <Link to={"/"}>
+        <Button
+          className="w-full"
+          btnText="Назад"
+        />
+      </Link>
+    </div>
+  );
+}
