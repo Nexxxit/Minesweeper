f097915905b9ea2e09e1cfca1039ed7fcdbe9b4d
Nexxxit <varsik.arsik@mail.ru>
2025-03-22 11:53:03 +0500

    feat: add opening cells

--- src/components/cell/Cell.tsx
+++ src/components/cell/Cell.tsx
@@ -1,21 +1,79 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface CellProps {
   id: number;
   col: number;
   row: number;
   hasBomb: boolean;
+  bombCount: number;
+  checkNearbyCells: (row: number, col: number) => void;
+  isOpenCell: Set<number>;
 }
 
-export default function Cell({ id, col, row, hasBomb }: CellProps) {
-  const [isOpen, setIsOpen] = useState(false);
+export default function Cell({
+  id,
+  col,
+  row,
+  hasBomb,
+  bombCount,
+  checkNearbyCells,
+  isOpenCell,
+}: CellProps) {
+  const [textColor, setTextColor] = useState<string>("");
+
+  useEffect(() => {
+    changeTextColor(bombCount);
+  }, [bombCount])
+
+  const changeTextColor = (count: number) => {
+    switch (count) {
+      case 1:
+        setTextColor("text-blue-500");
+        break;
+      case 2:
+        setTextColor("text-green-500");
+        break;
+      case 3:
+        setTextColor("text-red-500");
+        break;
+      case 4:
+        setTextColor("text-blue-800");
+        break;
+      case 5:
+        setTextColor("text-yellow-700");
+        break;
+      case 6:
+        setTextColor("text-cyan-400");
+        break;
+      case 7:
+        setTextColor("text-stone-950");
+        break;
+      case 8:
+        setTextColor("text-neutral-50");
+        break;
+      default:
+        setTextColor("");
+    }
+  };
+
+  const handleOpenCell = () => {
+    checkNearbyCells(row, col);
+  };
 
   return (
     <div
-      className="border border-3 shadow-xl border-gray-300 ring-1 w-full h-full flex items-center justify-center cursor-pointer"
-      onClick={() => setIsOpen(true)}
+      className={`border border-3 shadow-xl ${isOpenCell.has(id) ? "bg-gray-400" : ""} border-gray-300 ring-1 w-full h-full flex items-center justify-center cursor-pointer`}
+      onClick={handleOpenCell}
     >
-      {isOpen ? (hasBomb ? "💣" : `${row}, ${col}`) : "?"}
+      <p className={`${textColor} font-bold text-xl`}>
+        {isOpenCell.has(id)
+          ? hasBomb
+            ? "💣"
+            : bombCount !== undefined && bombCount > 0
+            ? `${bombCount}`
+            : ""
+          : ""}
+      </p>
     </div>
   );
 }
--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -6,6 +6,7 @@ interface CellData {
   row: number;
   col: number;
   hasBomb: boolean;
+  bombCount: number;
 }
 
 export default function Game() {
@@ -13,6 +14,8 @@ export default function Game() {
   const [rows, setRows] = useState<number>(8);
   const [cols, setCols] = useState<number>(8);
   const [cellData, setCellData] = useState<CellData[]>([]);
+  // const [bombAround, setBombAround] = useState<number>(0);
+  const [openedCell, setOpenedCell] = useState<Set<number>>(new Set());
 
   useEffect(() => {
     const gameMode = () => {
@@ -43,20 +46,88 @@ export default function Game() {
         const row = Math.floor(i / cols);
         const col = i % cols;
         let hasBomb = false;
-        if (Math.random() >= 0.6) {
+        if (Math.random() >= 0.8) {
           hasBomb = true;
         }
-        newCells.push({ id: i, row, col, hasBomb });
+        newCells.push({ id: i, row, col, hasBomb, bombCount: 0 });
       }
       setCellData(newCells);
     };
-  
+
     generateCells();
   }, [rows, cols]);
 
+  const direction = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+
+  const checkCells = (row: number, col: number): number => {
+    let count = 0;
+    direction.forEach(([dx, dy]) => {
+      let newRow = row + dx;
+      let newCol = col + dy;
+
+      if (0 <= newRow && newRow < rows && 0 <= newCol && newCol < cols) {
+        const cell = cellData.find(
+          (cell) => cell.row === newRow && cell.col === newCol
+        );
+        if (cell?.hasBomb) count++;
+      }
+    });
+    return count;
+  };
+
+  const openNearbyCells = (
+    row: number,
+    col: number,
+    temporaryOpened: Set<number>
+  ) => {
+    const currentCell = cellData.find(
+      (cell) => cell.row === row && cell.col === col
+    );
+
+    if (!currentCell || temporaryOpened.has(currentCell.id)) return;
+
+    temporaryOpened.add(currentCell.id);
+
+    const countBombs = checkCells(row, col);
+
+    if (countBombs > 0) return;
+
+    direction.forEach(([dx, dy]) => {
+      let newRow = row + dx;
+      let newCol = col + dy;
+
+      if (0 <= newRow && newRow < rows && 0 <= newCol && newCol < cols) {
+        openNearbyCells(newRow, newCol, temporaryOpened);
+      }
+    });
+  };
+
+  const checkNearbyCells = (row: number, col: number) => {
+    const temporaryOpened = new Set(openedCell);
+    openNearbyCells(row, col, temporaryOpened);
+
+    const updatedCellData = cellData.map(cell => {
+      if (temporaryOpened.has(cell.id)) {
+        return {...cell, bombCount: checkCells(cell.row, cell.col)};
+      }
+      return cell;
+    })
+    setCellData(updatedCellData);
+    setOpenedCell(new Set(temporaryOpened));
+  };
+
   return (
     <div
-      className="grid gap-1 border rounded-lg bg-gray-400"
+      className="grid gap-1 border rounded-lg bg-gray-200"
       style={{
         gridTemplateColumns: `repeat(${cols}, minmax(50px, 1fr))`,
         gridTemplateRows: `repeat(${rows}, minmax(50px, 1fr))`,
@@ -69,6 +140,9 @@ export default function Game() {
           col={cell.col}
           row={cell.row}
           hasBomb={cell.hasBomb}
+          bombCount={cell.bombCount}
+          isOpenCell={openedCell}
+          checkNearbyCells={checkNearbyCells}
         />
       ))}
     </div>
