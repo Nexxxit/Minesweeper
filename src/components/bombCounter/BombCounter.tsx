f590495f36152ddfc2e411a11222f929f5c5c319
Nexxxit <varsik.arsik@mail.ru>
2025-03-22 17:56:11 +0500

    style: update font

---
+++ src/components/bombCounter/BombCounter.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+interface BombCounter {
+    bombCount: number;
+    flagsSet: number;
+}
+
+export default function BombCounter({bombCount, flagsSet}: BombCounter) {
+    const [bombsLeft, setBombsLeft] = useState<number>(bombCount);
+
+    useEffect(() => {
+        calculateBombs();
+    }, [bombCount, flagsSet])
+
+    const calculateBombs = () => {
+       setBombsLeft(bombCount - flagsSet);
+    }
+
+    return (
+        <p className="text-white font-bold text-3xl">💣{bombsLeft}</p>
+    );
+}
