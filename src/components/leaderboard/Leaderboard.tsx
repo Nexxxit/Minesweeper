96c40fa83e454c6c95e7964e1dc52d726f5e07f7
Nexxxit <varsik.arsik@mail.ru>
2025-03-26 16:34:52 +0500

    style: update bg leaderboard

--- src/components/leaderboard/Leaderboard.tsx
+++ src/components/leaderboard/Leaderboard.tsx
@@ -5,7 +5,7 @@ import { observer } from "mobx-react-lite";
 
 export default observer(function Leaderboard() {
   const listItems = leaderboardStore.formattedRecords.map((record, index) => (
-    <li key={`${record.name}-${index}`} className="flex flex-wrap sm:flex-nowrap sm:justify-none justify-between items-center bg-yellow-400 p-5 gap-5 rounded-xl">
+    <li key={`${record.name}-${index}`} className="flex shadow-md flex-wrap sm:flex-nowrap sm:justify-none justify-between items-center bg-yellow-400 p-5 gap-5 rounded-xl">
       <div className="text-white font-bold text-xl sm:text-2xl sm:flex-grow min-w-0">{record.name}</div>
       <div className={`text-center text-xl sm:text-2xl shrink-0 order-2 sm:order-none w-[80px] sm:w-[120px] text-${record.level === "easy" ? 'white' : record.level === "medium" ? 'blue-500' : "red-500"} font-bold`}>{record.level}</div>
       <div className="text-white font-bold text-end shrink-0 text-xl sm:text-2xl">{record.time}</div>
@@ -17,7 +17,7 @@ export default observer(function Leaderboard() {
       <p className="text-center text-3xl sm:text-5xl lg:text-6xl font-bold text-yellow-300">
         Таблица лидеров
       </p>
-      <div className="border rounded-xl p-1 w-full h-100 overflow-y-auto scrollbar-hide">
+      <div className="border rounded-xl p-1 w-full h-100 overflow-y-auto scrollbar-hide bg-white/50">
         <ol className="flex flex-col gap-1 ">{listItems}</ol>
       </div>
       <Link className="w-full" to={"/"}>
