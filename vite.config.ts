025e11d7abc4e4ab22222a9e6e3542be9f315308
Nexxxit <varsik.arsik@mail.ru>
2025-03-26 20:13:27 +0500

    build: add base to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Minesweeper/',
   plugins: [
     react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
